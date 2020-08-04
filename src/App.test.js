feba8651c954d2ba5e3c9f8ad432d242beda2fa1
Olivier Goulet <oliviergoulet5@gmail.com>
2020-08-04 03:34:50 -0400

    First test written

--- src/App.test.js
+++ src/App.test.js
@@ -1,20 +1,18 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import { shallow } from 'enzyme';
+import { configure, shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
 import FeedbackForm from './FeedbackForm';
 
-
-
-test('Fake test', () => {
-    expect(true).toBeTruthy();
-});
+configure({ adapter: new Adapter() });
 
 describe('Interactions with the anonymity toggle on the form', () => {
-    test.skip("Hides the anonymity toggle when allowAnonymity configuration is false.", () => {
+    test("Hides the anonymity toggle when allowAnonymity configuration is false.", () => {
         const wrapper = shallow(
             <FeedbackForm configuration={{ allowAnonymity: false }} />
         )
     
-        wrapper.find('#anonymity-toggle').exists().toBe(false);  
+        expect(wrapper.find('#anonymity-toggle').isEmptyRender()).toBe(true)
     });
 })
